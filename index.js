099f0998cb05bcb76be2289780717bbe0a678dce
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2022-05-19 18:57:51 -0300

    fazendo correções

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
+import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-    return string;
+function pegaArquivo(caminhoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto));
+    })
 }
-
-console.log(texto(paragrafo));
+pegaArquivo('./arquivos/texto1.md');
