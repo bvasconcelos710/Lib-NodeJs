3c53c683403fa35ff04a52c5b80b8adcf1312625
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2022-05-24 18:23:03 -0300

    Utilizando promises, try/catch e async/await

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function pegaArquivo(caminhoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto));
-    })
+function trataErro(erro) {
+    throw new Error(chalk.red(erro));
 }
+
+async function pegaArquivo(caminhoArquivo) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(texto))
+    }
+    catch (erro) {
+        trataErro(erro);
+
+    }
+}
+
+//function pegaArquivo(caminhoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises.readFile(caminhoArquivo, encoding).then((texto) => console.log(chalk.green(texto))).catch((erro) => trataErro(erro));
+//}
+
+
+// function pegaArquivo(caminhoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+//        if (erro) {
+//           trataErro(erro);
+//        }
+//        console.log(chalk.green(texto));
+//   })
+//}
+
 pegaArquivo('./arquivos/texto1.md');
