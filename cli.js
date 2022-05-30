3cea080aed843271dedd468d15ae14b27eaf0ba8
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2022-05-30 17:30:33 -0300

    validando links

--- cli.js
+++ cli.js
@@ -2,12 +2,18 @@
 import chalk from 'chalk';
 import pegaArquivo from './index.js';
 //const pegaArquivo = require('./index.js');
+import validaURLs from './http_validacao.js';
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+    if (caminho[3] === 'validar') {
+        console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+    }
+    else {
+        console.log(chalk.yellow('lista de links'), resultado);
+    }
 }
 
 processaTexto(caminho);
